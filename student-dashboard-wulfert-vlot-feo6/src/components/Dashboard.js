11822743795a4dd3c7b7f444bae6b53e651791cf
Wulfert Vlot <wulfert93@gmail.com>
2020-07-24 15:02:37 +0200

    diagram per person available

--- student-dashboard-wulfert-vlot-feo6/src/components/Dashboard.js
+++ student-dashboard-wulfert-vlot-feo6/src/components/Dashboard.js
@@ -11,6 +11,11 @@ import {
   } from "react-router-dom";
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {naam: average};
+        //this.handleClick = this.handleClick.bind(this);
+      }
     render() {
         const allNames = (Grading).map(item => (`Naam: ${item.Naam}`));
         const av = average;
@@ -29,23 +34,135 @@ class Dashboard extends Component {
         const Evelyn =  Grading.filter(function(deel) {
             return deel.Naam == "Evelyn";
         });
+        const Aranka =  Grading.filter(function(deel) {
+            return deel.Naam == "Aranka";
+        });
+        const Floris =  Grading.filter(function(deel) {
+            return deel.Naam == "Floris";
+        });
+        const Hector =  Grading.filter(function(deel) {
+            return deel.Naam == "Hector";
+        });
+        const Martina =  Grading.filter(function(deel) {
+            return deel.Naam == "Martina";
+        });
+        const Maurits =  Grading.filter(function(deel) {
+            return deel.Naam == "Maurits";
+        });
+        const Rahima =  Grading.filter(function(deel) {
+            return deel.Naam == "Rahima";
+        });
+        const Sandra =  Grading.filter(function(deel) {
+            return deel.Naam == "Sandra";
+        });
+        const Wietske =  Grading.filter(function(deel) {
+            return deel.Naam == "Wietske";
+        });
+        const Storm =  Grading.filter(function(deel) {
+            return deel.Naam == "Storm";
+        });
+        const handleClickEvlyn = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Evelyn});
+            event.preventDefault();
+        }
+        const handleClickAranka = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Aranka});
+            event.preventDefault();
+        }
+        const handleClickFloris = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Floris});
+            event.preventDefault();
+        }
+        const handleClickHector = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Hector});
+            event.preventDefault();
+        }
+        const handleClickMartina = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Martina});
+            event.preventDefault();
+        }
+        const handleClickMaurits = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Maurits});
+            event.preventDefault();
+        }
+        const handleClickRahima = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Rahima});
+            event.preventDefault();
+        }
+        const handleClickSandra = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Sandra});
+            event.preventDefault();
+        }
+        const handleClickWietske = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Wietske});
+            event.preventDefault();
+        }
+        const handleClickStorm = (event) => {
+            
+            //console.log(`The name was clicked and the current state equals: ${this.state.naam} and you clicked on: ${event.target.innerText}`);
+            //console.log(`${typeof Storm}, ${typeof event.target.innerText}`);
+            this.setState({naam: Storm});
+            event.preventDefault();
+        }
+        
+        console.log(`currentstate: ${this.state.naam}`);
+        // function handleClick(e) {
+        //     e.preventDefault();
+        //     console.log(`The link was clicked.: ${this.state.naam}`);
+        //   }
         return (
             <div>
                 <ul>
-                {namen.forEach(element => <li>{(element.naam)}</li>)}
-                </ul>
+                {/* {namen.forEach(item => console.log('banaan'))} */} 
+                <li onClick={handleClickEvlyn}>Evelyn</li>
+                <li onClick={handleClickAranka}>Aranka</li>
+                <li onClick={handleClickFloris}>Floris</li>
+                <li onClick={handleClickHector}>Hector</li>
+                <li onClick={handleClickMartina}>Martina</li>
+                <li onClick={handleClickRahima}>Rahima</li>
+                <li onClick={handleClickSandra}>Sandra</li>
+                <li onClick={handleClickWietske}>Wietske</li>
+                <li onClick={handleClickStorm}>Storm</li>
                 
+                </ul>
                 <h1>Dashboard</h1>
                 <h2>Moeilijkheid</h2>
                 <V.VictoryChart>
-                    <V.VictoryBar data={av}
+                    <V.VictoryBar data={this.state.naam}
                     x="Opdracht"
                     y="Leuk"
                     />
                     </V.VictoryChart>
                     <h2>Leukheid</h2>
                     <V.VictoryChart>
-                    <V.VictoryBar data={av}
+                    <V.VictoryBar data={this.state.naam}
                     x="Opdracht"
                     y="Moeilijk"
                     />
